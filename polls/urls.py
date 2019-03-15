4d450d2e21eced9361e4d1be6d4218f232b46c28
Igor <igorsamojlov88@gmail.com>
2019-03-15 22:14:43 +0300

    Lesson 4

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,14 @@
-from django.contrlib import admin
+#from django.contrlib import admin
 from django.urls import path
+from . import views
+
+app_name = 'polls'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote', views.vote, name='vote'),
+
 ]
