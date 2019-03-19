56d98d5f91987b89dcc9c0dc491ed2a42310beae
Igor <igorsamojlov88@gmail.com>
2019-03-19 23:45:13 +0300

    5-6 lesson

--- polls/views.py
+++ polls/views.py
@@ -1,26 +1,48 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import loader
-from django.http import Http404
 from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choise
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list':latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return (render(request, 'polls/detail.html', {'question':question}))
+    def get_queryset(self):
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/result.html', {'question':question})
+# def index(request):
+#    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#    template = loader.get_template('polls/index.html')
+#    context = {
+#        'latest_question_list':latest_question_list,
+#    }
+#    return HttpResponse(template.render(context, request))
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+"""
+GET /users - index user
+GET /users/:id - show user
+POST /users - create new user
+PUT /users/:id - udpate exiting user
+DELTE /users/:id - destroy user
+
+GET /users/new - render user new form
+GET /users/:id/edit - render edit existing user form
+
+"""
+
 
 def vote (request, question_id):
     question = get_object_or_404(Question, pk=question_id)
